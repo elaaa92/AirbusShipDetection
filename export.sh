6e53eea244f58b51271d0982a4a75881828dd0e5
elaaa92 <eladomus07@gmail.com>
2018-11-16 18:56:49 +0100

    Bug fixes

--- export.sh
+++
@@ -1,1 +0,0 @@
-export PYTHONPATH=:/usr/local/lib/python2.7/dist-packages/tensorflow/models/research:/usr/local/lib/python2.7/dist-packages/tensorflow/models/research/slim
--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./export.sh
+export PYTHONPATH=:/usr/local/lib/python2.7/dist-packages/tensorflow/models/research:/usr/local/lib/python2.7/dist-packages/tensorflow/models/research/slim
 
 PROJECT_DIRECTORY=`pwd`
 

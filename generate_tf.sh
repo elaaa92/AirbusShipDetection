89c4c7fb5b62f5e99cba2d86303fb6b4ab9a5209
elaaa92 <eladomus07@gmail.com>
2018-11-16 09:09:13 +0100

    Added bash launcher for file generation

---
+++ generate_tf.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export PYTHONPATH=:/usr/local/lib/python2.7/dist-packages/tensorflow/models/research:/usr/local/lib/python2.7/dist-packages/tensorflow/models/research/slim:/usr/local/lib/python2.7/dist-packages/tensorflow/models/research:/usr/local/lib/python2.7/dist-packages/tensorflow/models/research/slim:/usr/local/lib/python2.7/dist-packages/tensorflow/models/research:/usr/local/lib/python2.7/dist-packages/tensorflow/models/research/slim
+
+python generate_tf.py

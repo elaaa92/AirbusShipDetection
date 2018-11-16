2855359282fd4b8d2a93a28f89326ddb0a5fe154
elaaa92 <eladomus07@gmail.com>
2018-11-16 19:18:27 +0100

    Bug fixes

--- eval.sh
+++ eval.sh
@@ -13,9 +13,8 @@ cd /usr/local/lib/python2.7/dist-packages/tensorflow/models/research/object_dete
 
 for i in "${ckpts[@]}"
 do
-   CHECKPOINT_DIR=$MODEL_DIRECTORY/fine_tuned_model-$i
-   EVAL_DIR=$MODEL_DIRECTORY/eval
-   PIPELINE_CONFIG_PATH=$MODEL_DIRECTORY/fine_tuned_model-$i/pipeline.config
-
-   python eval.py --logtostderr --checkpoint_dir=${CHECKPOINT_DIR} --eval_dir=${EVAL_DIR} --pipeline_config_path=${PIPELINE_CONFIG_PATH}
+	CHECKPOINT_DIR=$MODEL_DIRECTORY/fine_tuned_model-$i
+	EVAL_DIR=$MODEL_DIRECTORY/eval
+	PIPELINE_CONFIG_PATH=$MODEL_DIRECTORY/fine_tuned_model-$i/pipeline.config
+	python eval.py --logtostderr --checkpoint_dir=${CHECKPOINT_DIR} --eval_dir=${EVAL_DIR} --pipeline_config_path=${PIPELINE_CONFIG_PATH}
 done
